bf020f0c550d55e6020d56d7bc0a83d91c095ef7
grna <kgirius@gmail.com>
2021-10-24 22:34:35 +0300

    login component created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const LogIn = (props) => {
+const LogIn = ({ userLogIn }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogInSubmit = (e) => {
+    e.preventDefault();
+    const form = {
+      email: email,
+      password: password,
+    };
+    userLogIn(form);
+  };
+
   return (
     <div>
-      <form></form>
+      <form onSubmit={(e) => onLogInSubmit(e)}>
+        <label>Email:</label>
+        <input
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
+        <input
+          type="submit"
+          className="btn btn-lg"
+          value="LogIn"
+        ></input>
+      </form>
     </div>
   );
 };
 
-LogIn.propTypes = {};
+LogIn.propTypes = {
+  userLogIn: PropTypes.func,
+};
 
 export default LogIn;
